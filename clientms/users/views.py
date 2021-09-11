6bb08e7f3a8037ca3b2126ff4c83fdbeb48e4cf2
Zachary Muehlich <zmuehlich@unomaha.edu>
2021-09-11 11:46:32 -0500

    commit

--- clientms/users/views.py
+++ clientms/users/views.py
@@ -1,9 +1,17 @@
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.views import PasswordChangeView
 
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class ChangePwView(LoginRequiredMixin, PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/change_password.html'
